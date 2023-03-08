d0867b5e35b2b72e804a5f4d039db8b48f7a520e
Alex Grant <alexgrantjnr@yahoo.co.uk>
2023-03-08 01:06:53 +0000

    update

--- dayCalulation.py
+++ dayCalulation.py
@@ -2,10 +2,14 @@ minute = 60
 hour = 60
 day = 24
 
+ans = input("Calculate seconds per day or hour enter day/hour")
+
 sec_per_hur = minute * hour
 sec_per_day = day * sec_per_hur
 
-print(sec_per_hur)
-print(sec_per_day)
-print(sec_per_day / sec_per_hur)
-print(sec_per_day // sec_per_hur)
+if ans == "day":
+    print(sec_per_hur)
+else:
+    print(sec_per_day)
+    print(sec_per_day / sec_per_hur)
+    print(sec_per_day // sec_per_hur)
